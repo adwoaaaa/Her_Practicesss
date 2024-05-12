667f735d7e1b9a66f5954b51dafd4545100ef9cb
adwoaaaa <constancemensah05@gmail.com>
2024-05-11 13:45:57 -1200

    Tasks on java functions
    ,

---
+++ Absolutesss.java
@@ -0,0 +1,20 @@
+public class Absolutesss
+{
+public static double abs (double a)
+{
+    if (a < 0){
+        return Math.abs(a);
+    }else return a;
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ mainnn.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class mainnn
+{
+public static void main (String[] args)
+{
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    double a = input.nextDouble();
+    double absolute = Math.abs(a);
+    System.out.println("The absolute value of the numbers is :" + absolute);
+
+
+
+
+
+
+
+}
+}
