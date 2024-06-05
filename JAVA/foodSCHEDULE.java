5199645209490f6577f8b6572cb33018bb118915
MVRK0 <elikemfenuksu@gmail.com>
2024-06-05 00:40:00 +0000

    refactor: minor changes

--- JAVA/foodSCHEDULE.java
+++ JAVA/foodSCHEDULE.java
@@ -1,6 +1,5 @@
-import java.util.Scanner;
-import java.util.Random;
-
+package JAVA;
+import java.util.*;
 public class foodSCHEDULE{
 
     public static void main(String[] args) {
@@ -10,26 +9,24 @@ public class foodSCHEDULE{
         String[] supper = {"FUFU AND PALMNUT SOUP", "BANKU WITH TILAPIA", "KENKEY AND FRIED FISH", "RICE AND STEW", "INDOMIE"};
 
         System.out.println("Hey Adwoa, it is time for food. Would you like to take Breakfast, Lunch or Supper? ");
-        String food = input.next();
+        String food = input.next().toLowerCase();
 
-        if (food.equals ("Breakfast")) {
-        Random random = new Random();
-        int randomIndex = random.nextInt(breakfast.length);
-        String randomString = breakfast[randomIndex];
-        System.out.println("This morning; for breakfast, you would take " + randomString + ".");
-    }else if (food.equals ("Lunch"))  {
-        Random random = new Random();
-        int randomIndex = random.nextInt(lunch.length);
-        String randomString = lunch[randomIndex];
-        System.out.println("This afternoon; for lunch, you would take " + randomString + ".");
-    } 
-    else
-    {
-         Random random = new Random();
-        int randomIndex = random.nextInt(supper.length);
-        String randomString = supper[randomIndex];
-        System.out.println("This evening; for supper, you would take " + randomString + ".");
-    }
+        if(food.equals ("breakfast")){
+            Random random = new Random();
+            int randomIndex = random.nextInt(breakfast.length);
+            String randomString = breakfast[randomIndex];
+            System.out.println("This morning; for breakfast, you would take " + randomString + ".");
+        } else if (food.equals ("lunch"))  {
+            Random random = new Random();
+            int randomIndex = random.nextInt(lunch.length);
+            String randomString = lunch[randomIndex];
+            System.out.println("This afternoon; for lunch, you would take " + randomString + ".");
+        }else{
+            Random random = new Random();
+            int randomIndex = random.nextInt(supper.length);
+            String randomString = supper[randomIndex];
+            System.out.println("This evening; for supper, you would take " + randomString + ".");
+        }
     }
 }    
 
--- JAVA/guessingGAME.java
+++ JAVA/guessingGAME.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
+package JAVA;
 
-public class guessingGAME
-{
-    public static void main(String[] args)
-     {
+import java.util.Scanner;
+public class guessingGAME {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int randomNumber = (int)(Math.random() * 20) + 1;
-
-       // Generates a Random number between 1 and 20 for the guessing game 
-
+        // Generates a Random number between 1 and 20 for the guessing game
         System.out.println("WELCOME TO MY GUESSING GAME");
         System.out.println("");
         System.out.println("Guess a number from 1 to 20 ");
@@ -18,26 +15,21 @@ public class guessingGAME
         boolean guess = false;
 
         while (!guess){
-         System.out.print("Enter your number: ");
-
-         int number = input.nextInt();
-                 attempts++;
-
-         if (number == randomNumber){
-            System.out.println("GENIUS! You guessed correctly.");
-            System.out.println("Total number of attempts: " + attempts);
-                guess = true;
-                
-               } else if (number > 20) {
-                  System.out.println("Sorry, the number you guessed is not within 1 to 20.");
-          } else if (number > randomNumber) {
-               System.out.println("OPPS! The number you guessed is too high, keep trying.");
-          
-               }else {
-                  System.out.println("OPPS! The number you guessed is too low, keep trying.");
-      
-         }
-    }        
+            System.out.print("Enter your number: ");
+             int number = input.nextInt();
+                     attempts++;
+             if (number == randomNumber){
+                System.out.println("GENIUS! You guessed correctly.");
+                System.out.println("Total number of attempts: " + attempts);
+                    guess = true;
+             }else if (number > 20) {
+                 System.out.println("Sorry, the number you guessed is not within 1 to 20.");
+             } else if (number > randomNumber) {
+                 System.out.println("OPPS! The number you guessed is too high, keep trying.");
+             }else {
+                 System.out.println("OPPS! The number you guessed is too low, keep trying.");
+             }
+        }
    
-   }
+    }
 }
